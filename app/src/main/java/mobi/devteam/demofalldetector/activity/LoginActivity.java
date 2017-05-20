c96354101b08a6df75c17bc6ffc4124ca9a267ec
Minh Nguyen <minh.nguyenvan95@gmail.com>
2017-05-20 21:41:55 +0700

    Clear reminder nav, add Butterknife lib

--- app/src/main/java/mobi/devteam/demofalldetector/activity/LoginActivity.java
+++ app/src/main/java/mobi/devteam/demofalldetector/activity/LoginActivity.java
@@ -1,15 +1,30 @@
 package mobi.devteam.demofalldetector.activity;
 
+import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.widget.Button;
 
+import butterknife.BindView;
+import butterknife.ButterKnife;
+import butterknife.OnClick;
+import butterknife.OnItemClick;
 import mobi.devteam.demofalldetector.R;
 
 public class LoginActivity extends AppCompatActivity {
+
+    @BindView(R.id.btnSignIn) Button btnSignIn;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_login);
+        ButterKnife.bind(this);
+    }
+
+    @OnClick(R.id.btnSignIn) void sign_in(){
+        //TODO: fix this later
+        Intent intent = new Intent(LoginActivity.this,MainActivity.class);
+        startActivity(intent);
     }
 }
