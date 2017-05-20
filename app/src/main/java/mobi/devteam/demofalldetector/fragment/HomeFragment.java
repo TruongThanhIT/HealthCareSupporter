012008e2ac231c30393f914dc4b934a3a2cc1531
Minh Nguyen <minh.nguyenvan95@gmail.com>
2017-05-20 22:20:03 +0700

    HCS 9: add image , fragment

---
+++ app/src/main/java/mobi/devteam/demofalldetector/fragment/HomeFragment.java
@@ -0,0 +1,45 @@
+package mobi.devteam.demofalldetector.fragment;
+
+import android.content.Context;
+import android.net.Uri;
+import android.os.Bundle;
+import android.support.v4.app.Fragment;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
+import butterknife.OnClick;
+import mobi.devteam.demofalldetector.R;
+
+public class HomeFragment extends Fragment {
+
+     public HomeFragment() {
+
+    }
+
+    public static HomeFragment newInstance() {
+        HomeFragment fragment = new HomeFragment();
+        Bundle args = new Bundle();
+        fragment.setArguments(args);
+        return fragment;
+    }
+
+    @Override
+    public void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        if (getArguments() != null) {
+
+        }
+    }
+
+    @Override
+    public View onCreateView(LayoutInflater inflater, ViewGroup container,
+                             Bundle savedInstanceState) {
+        // Inflate the layout for this fragment
+        return inflater.inflate(R.layout.fragment_home, container, false);
+    }
+
+    @OnClick(R.id.fab_add) void fab_onclick(){
+        //TODO: start add reminder activity
+    }
+}
